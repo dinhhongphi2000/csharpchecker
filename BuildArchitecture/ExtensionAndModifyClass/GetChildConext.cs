221107b613b82854667154cdfe73977da27f97b1
Dinh Hong Phi <dinhhongphi@gmail.com>
2018-12-15 15:00:08 +0700

    add get deep child context

---
+++ BuildArchitecture/ExtensionAndModifyClass/GetChildConext.cs
@@ -0,0 +1,36 @@
+﻿using Antlr4.Runtime;
+using Antlr4.Runtime.Misc;
+using Antlr4.Runtime.Tree;
+using System.Collections.Generic;
+
+namespace BuildArchitecture
+{
+    public static class GetChildConext
+    {
+        public static List<T> GetDeepChildContext<T>(this ParserRuleContext context)
+            where T : ParserRuleContext
+        {
+            ContextListener<T> listener = new ContextListener<T>();
+            ParseTreeWalker walker = new ParseTreeWalker();
+            walker.Walk(listener, context);
+            return listener.ContextList;
+        }
+    }
+
+    public class ContextListener<T> : CSharpParserBaseListener where T : ParserRuleContext
+    {
+        public List<T> ContextList { get; set; }
+
+        public ContextListener()
+        {
+            ContextList = new List<T>();
+        }
+
+        public override void EnterEveryRule([NotNull] ParserRuleContext context)
+        {
+            if (context is T)
+                ContextList.Add((T)context);
+            base.EnterEveryRule(context);
+        }
+    }
+}
