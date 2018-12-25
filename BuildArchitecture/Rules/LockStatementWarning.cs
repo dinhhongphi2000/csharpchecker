10fa842f3689ecf75dca4c50b1d77417d54def8f
gnort <trong.hoduc@gmail.com>
2018-12-26 01:31:13 +0700

    add check lock

---
+++ BuildArchitecture/Rules/LockStatementWarning.cs
@@ -0,0 +1,41 @@
+﻿using Antlr4.Runtime;
+using BuildArchitecture.Semetic.V2;
+using System;
+using System.ComponentModel.Composition;
+using static BuildArchitecture.CSharpParser;
+
+namespace BuildArchitecture.Rules
+{
+    class LockStatementWarning
+    {
+        [Export(typeof(LockStatementContext))]
+        public void CheckLockStatement(ParserRuleContext context, out ErrorInformation error)
+        {
+            error = null;
+            var priExpressionContext = context.GetDeepChildContext<Primary_expressionContext>()[0];
+            if(priExpressionContext.GetText() == "this")
+            {
+                error = ThrowError(priExpressionContext);
+
+            }
+            else 
+            {
+                var typeofContext = priExpressionContext.GetDeepChildContext<TypeofExpressionContext>();
+                if(typeofContext.Count > 0)
+                {
+                    error = ThrowError(priExpressionContext);
+                }
+            }
+        }
+        public ErrorInformation ThrowError(ParserRuleContext context)
+        {
+           return new ErrorInformation
+            {
+                StartIndex = context.Start.StartIndex,
+                ErrorCode = "WA0006",
+                Length = context.Stop.StopIndex - context.Start.StartIndex + 1,
+                ErrorMessage = "UIT: Types and this should not be used for locking "
+           };
+        }
+    }
+}
